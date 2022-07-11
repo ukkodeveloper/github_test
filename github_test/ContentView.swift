4ca4fb95358adc0270628f3b052d37793f11e7cd
ukkodeveloper <ukkodeveloper@gmail.com>
2022-07-11 12:34:58 +0900

    Initial Commit

---
+++ github_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  github_test
+//
+//  Created by 김유권 on 2022/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ github_test/github_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_testApp.swift
+//  github_test
+//
+//  Created by 김유권 on 2022/07/11.
+//
+
+import SwiftUI
+
+@main
+struct github_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
